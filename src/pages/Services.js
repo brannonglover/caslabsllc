959ad9e17696995ac6db06ecd258535f3c5dd5f5
Brannon Glover <brannon.glover@warnermedia.com>
2024-06-14 12:35:27 -0400

    Moved api key to env

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,17 +1,31 @@
+import { useState } from 'react';
 import '../styles/main.scss';
 import services from '../assets/services.json';
 
 function Services() {
+  const [output, setOutput] = useState(null);
+  
+  function handleDropdown(e) {
+    Object.keys(services).forEach(service => {
+      if (service === e.target.value) {
+        setOutput(services[service]);
+      }
+    })
+  }
+
   return (
     <div className="main-content">
-      <select>
-        <option>select service...</option>
-        {Object.keys(services).map(service => {
-          return (
-            <option value={service}>{service}</option>
-          )
-        })}
-      </select>
+      <div className='services'>
+        <select onChange={handleDropdown}>
+          <option>select service...</option>
+          {Object.keys(services).map(service => {
+            return (
+              <option value={service}>{service}</option>
+            )
+          })}
+        </select>
+      </div>
+      <div className='services-output' dangerouslySetInnerHTML={{__html: output}}></div>
     </div>
   );
 }
