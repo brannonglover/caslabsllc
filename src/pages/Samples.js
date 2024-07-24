9abb42ac85e50d47b0626cbcb3a510df1aaecfcf
Brannon Glover <brannon.glover@warnermedia.com>
2024-07-24 10:47:17 -0400

    Added samples pdf

--- src/pages/Samples.js
+++ src/pages/Samples.js
@@ -1,3 +1,4 @@
+import pdf from '../images/pdf-icon.png';
 import '../styles/main.scss';
 
 function Samples() {
@@ -8,7 +9,17 @@ function Samples() {
         <p>When submitting samples, please identify your samples properly, along with the specific test to be performed. Please provide estimated expected levels when possible. Please provide company information. Coolers can be returned if requested. Samples are retained for 30 days, unless you send other instructions.</p>
         <p>Some samples require additional preparation for analysis and may have a preparation charge. Our testing is only representative of the sample submitted, please use proper sampling techniques.</p>
         <p>Click here to download the Sample Submission Form or click the PDF logo below.</p>
-        <p>All results are strictly confidential and are not released without our client’s permission. Please call or email our customer service for more information.</p>
+        <p>All results are strictly confidential and are not released without our client's permission. Please call or email our customer service for more information.</p>
+        <div className="pdf">
+          <div className='pdf-icon'>
+            <img src={pdf} alt="PDF" />
+          </div>
+          <div>
+            <a href="/assets/Carolina-Analytical-Services-Sample-Submission-Form-2021.pdf" title="Carolina Analytical Services Sample Form">Carolina-Analytical-Services-Sample-Submission-Form-2021.pdf</a><br />
+            Size: 223.417 Kb<br />
+            Type: pdf
+          </div>
+        </div>
       </div>
     </div>
   );
