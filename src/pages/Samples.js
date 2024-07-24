8c8bc46741b7a86997e81ed6bb2d5ab050098be4
Brannon Glover <brannon.glover@warnermedia.com>
2024-07-24 10:52:09 -0400

    changed pdf location

--- src/pages/Samples.js
+++ src/pages/Samples.js
@@ -15,7 +15,7 @@ function Samples() {
             <img src={pdf} alt="PDF" />
           </div>
           <div>
-            <a href="../assets/Carolina-Analytical-Services-Sample-Submission-Form-2021.pdf" title="Carolina Analytical Services Sample Form">Carolina-Analytical-Services-Sample-Submission-Form-2021.pdf</a><br />
+            <a href="/Carolina-Analytical-Services-Sample-Submission-Form-2021.pdf" title="Carolina Analytical Services Sample Form">Carolina-Analytical-Services-Sample-Submission-Form-2021.pdf</a><br />
             Size: 223.417 Kb<br />
             Type: pdf
           </div>
